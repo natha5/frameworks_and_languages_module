70cfbdeec87c25b6ee67dc54f36664c153349cc1
natha5 <ns539@canterbury.ac.uk>
2021-11-15 15:36:36 +0000

    Began building get request

--- server/app.py
+++ server/app.py
@@ -1,3 +1,23 @@
 import falcon
 
-app = application = falcon.app
+import json
+
+
+class resource
+    def on_get(self, req, resp):
+        """Handles GET requests"""
+        resp.media = {'message': 'hello world'}
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+    
+"""    def on_post()
+
+
+    def on_delete()"""
+
+    resp.text = json.dumps()    
+
+api = falcon.API()
+
+app =  = falcon.app()
+app.add_route('/', resource())
