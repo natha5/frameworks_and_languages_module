e371fd307b77884423a3e6661bc42d0b33c8d591
natha5 <ns539@canterbury.ac.uk>
2021-11-22 14:46:50 +0000

    Added lines for wsgiref server

--- server/app.py
+++ server/app.py
@@ -2,22 +2,41 @@ import falcon
 
 import json
 
+from wsgiref import simple_server
+from datastore import ITEMS
 
-class resource
+class resource:
     def on_get(self, req, resp):
         """Handles GET requests"""
-        resp.media = {'message': 'hello world'}
+        if req.get_param("id"):
+            resp.media = {'user_id': id, "keywords":[],"description": "", "lat": "" , "lon": "" }
+        
         resp.status = falcon.HTTP_200
         resp.content_type = falcon.MEDIA_JSON
     
-"""    def on_post()
+   
+    def on_post(self, req, resp):
+        """Handles POST requests"""
+
+        resp.media = {"id" : ITEMS.id, 'user_id' : ITEMS.user_id, 'description' : }
+
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
 
 
-    def on_delete()"""
 
-    resp.text = json.dumps()    
+    def on_delete(self, req, resp):
+        """Handles DELETE requests"""
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+        
 
-api = falcon.API()
+    
 
-app =  = falcon.app()
+api = openapi.yml
+
+app = application = falcon.App()
 app.add_route('/', resource())
+
+server = make_server('localhost', 8000, application)
+server.serve_forever()
