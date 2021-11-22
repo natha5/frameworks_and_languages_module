9f647091f602406a973f6f810ff6f86b429e2c78
natha5 <ns539@canterbury.ac.uk>
2021-11-22 11:45:05 +0000

    Added data store file with basic dicitionary for data

---
+++ server/dataStore.py
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+ITEMS = {
+    1: {
+        "id": 1,
+        "user_id": "user1234",
+        "keywords": ["hammer", "nails", "tools"],
+        "description": "A hammer and nails set",
+        "lat": 1,
+        "lon": 1,
+        "date_from": "2021-11-22T08:22:39.067408",
+    }
+}
+NEXT_ID = max(ITEMS.keys()) + 1
