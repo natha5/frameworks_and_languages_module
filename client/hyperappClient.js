5831d0f38a2eea821b24de10632857001d6ecf1b
natha5 <ns539@canterbury.ac.uk>
2021-11-29 11:44:12 +0000

    Added client javascript file

---
+++ client/hyperappClient.js
@@ -0,0 +1,10 @@
+import {h, text, app} from "https://cdn.skypack.dev/hyperapp"
+
+app({
+  view: () => h("main", {}, [
+    h("div", {class: "person"}, [
+      h("p", {}, text("Hello world")),
+    ]),
+  ]),
+  node: document.getElementById("app"),
+})
