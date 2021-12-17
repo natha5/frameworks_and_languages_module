f6aa50b54ba53b4139305c56489a76b38042a6b9
natha5 <nathan.j.shuttleworth@gmail.com>
2021-12-17 15:25:59 +0000

    Edited Cors handling and got incorrect post fields test to pass

--- server/app.py
+++ server/app.py
@@ -23,6 +23,7 @@ class ItemResource:
         resp.status = falcon.HTTP_200
         resp.content_type = falcon.MEDIA_JSON
 
+
 class MultipleItemsResource:
     def on_get(self, req, resp):
         """Handles GET request for multiple items"""
@@ -38,35 +39,30 @@ class MultipleItemsResource:
 class PostResource:
     def on_post(self, req, resp):
         """Handles POST requests"""
-        app = json.load(req.bounded_stream)
+        inputData = json.load(req.bounded_stream)
         
         #data = req.JSON
 
         #create new values that arent inputted by user
         newDateFrom = datetime.datetime.now()
         newDateTo = datetime.datetime.now()
-        newid = max(ITEMS.keys()) + 1
+        newId = max(ITEMS.keys()) + 1
 
-        ## check got the right fields
-        
-        
+        ## check the correct fields have been filled
 
-        fields = set({'user_id', 'keywords', 'description', 'image', 'lat', 'lon'})
+        fields = set({'user_id', 'keywords', 'description', 'image', 'lat', 'lon', 'date_from', 'date_to'})
 
         if(ITEMS.keys != fields):
-            resp.status = falcon.HTTP_204
+            resp.status = falcon.HTTP_405
         else:
-            ITEMS[newid] = {
-                "id" : newid,
-                "user_id" : req.user_id,
-                "keywords" : req.keywords,
-                "description" : req.description,
-                
-                "image" : req.image,
-                "lat" : req.lat,
-                "lon" : req.lon,
-                
-                
+            ITEMS[newId] = {
+                "id" : newId,
+                "user_id" : inputData.user_id,
+                "keywords" : inputData.keywords,
+                "description" : inputData.description,
+                "image" : inputData.image,
+                "lat" : inputData.lat,
+                "lon" : inputData.lon,
                 "date_from" : newDateFrom,
                 "date_to ": newDateTo
             }
@@ -84,26 +80,28 @@ class PostResource:
 
 class rootResource:
     def on_get(self, resp, req):
-        resp.status = falcon. HTTP_200
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+
 
 
 
     
 
-#class OptionsResource:
-#    def on_options(self, req, resp):
-#
-#        resp.status = falcon.HTTP_200
-#        resp.content_type = falcon.MEDIA_JSON
+class OptionsResource:
+    def on_options(self, req, resp):
 
-app = application = falcon.App(cors_enable=True)
+        resp.status = falcon.HTTP_204
+        resp.content_type = falcon.MEDIA_JSON
 
 
 #cors handling
+app = falcon.App(cors_enable=True)
+
 app = falcon.App(middleware=falcon.CORSMiddleware(
-    allow_origins= '*', 
-    allow_credentials='*', 
-    expose_headers= 'content-type'))
+    allow_origins='*', 
+    allow_credentials='*',
+    expose_headers='None'))
 
 
 
